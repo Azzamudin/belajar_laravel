7718e450d4d1dc5b75121de1c651c51bff83e9c0
azzamudin <azzamudin.arrahyi94@gmail.com>
2020-09-11 22:52:19 +0700

    mengubah isi file blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laravel</title>
 </head>
 <body>
-    <h1>Selamat Datang di Tutorial Malas Ngoding</h1>
+    <h1>WWW.MALASNGODING.COM</h1>
+    <P>selamat Datang di Tutorial Laravel www.malasngoding.com</P>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt hic, recusandae exercitationem illum sed repellendus aperiam nisi! Suscipit inventore at iusto nulla. Ad unde pariatur esse non, exercitationem hic cumque?</p>
 </body>
 </html>
