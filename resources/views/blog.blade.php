435a21b39687eb46b3ec6923c7f9da25566406d9
azzamudin <azzamudin.arrahyi94@gmail.com>
2020-09-11 22:49:07 +0700

    menambahkan file blog.blade.php

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Selamat Datang di Tutorial Malas Ngoding</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt hic, recusandae exercitationem illum sed repellendus aperiam nisi! Suscipit inventore at iusto nulla. Ad unde pariatur esse non, exercitationem hic cumque?</p>
+</body>
+</html>
