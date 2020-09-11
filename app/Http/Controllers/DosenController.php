e6557b2890bf08f99dd78db6a08c5778a9e7d8b8
azzamudin <azzamudin.arrahyi94@gmail.com>
2020-09-12 00:21:20 +0700

    mengubah konten file dosencontroller.php

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DosenController extends Controller
 {
     public function index(){
-        return "Halo ini adalah method index, dalam controller controller DosenController. - www.malasngoding.com";
+        return view('biodata');
     }
 }
