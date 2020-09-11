013dc45036ddea803c1003afc18442354261ced7
azzamudin <azzamudin.arrahyi94@gmail.com>
2020-09-11 23:58:04 +0700

    menambahkan konten pada file dosencontroller.php

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class DosenController extends Controller
 {
-    //
+    public function index(){
+        return "Halo ini adalah method index, dalam controller controller DosenController. - www.malasngoding.com";
+    }
 }
