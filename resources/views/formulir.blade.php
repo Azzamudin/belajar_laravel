55e4122ac50d3a35da95aa14f759e45dbbb8a885
azzamudin <azzamudin.arrahyi94@gmail.com>
2020-09-12 14:12:04 +0700

    menambahkan sub file formulir.blade.php pada resources views

---
+++ resources/views/formulir.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+
+    <form action="/formulir/proses" method="post">
+        <input type = "hidden" name = "_token" value = "<?php echo csrf_token() ?>">
+
+        Nama:
+        <input type="text" name="nama"> </br>
+        Alamat:
+        <input type="text" name="alamat"> </br>
+        <input type="submit" value="simpan"> </br>
+    </form>
+
+</body>
+</html>
