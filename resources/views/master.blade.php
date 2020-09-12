abae76851a1cadd1cf3d15e9f0fb8954f5152a19
azzamudin <azzamudin.arrahyi94@gmail.com>
2020-09-13 00:49:17 +0700

    mengubah konten master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutorial Laravel #8 : Sistem Template Blade Laravel</title>
 </head>
 <body>
+
     <header>
-        <h2>Blog Malas Ngoging</h2>
+
+        <h2>Blog MalasNgoding</h2>
         <nav>
             <a href="/blog">HOME</a>
 
@@ -20,8 +20,8 @@
     <br/>
     <br/>
 
-    <!--bagian judul blog -->
-    <h3> @yeild('judul_halaman') </h3>
+    <!--bagian judul halaman blog -->
+    <h3> @yield('judul_halaman') </h3>
 
     <!-- bagian konten blog -->
     @yield('konten')
