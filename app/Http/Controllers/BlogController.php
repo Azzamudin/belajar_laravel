004b4a964aef1e357925913441f04115c1e936a9
azzamudin <azzamudin.arrahyi94@gmail.com>
2020-09-13 00:48:32 +0700

    merubah konten blogcontroller.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,17 +6,17 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    
-    public function home(){
-        return view('home');
-    }
 
-    public function tentang(){
-        return view('tentang');
-    }
+	public function home(){
+		return view('home');
+	}
 
-    public function kontak(){
-        return view('kontak')
-    }
-    
+	public function tentang(){
+		return view('tentang');
+	}
+
+	public function kontak(){
+		return view('kontak');
+	}
+
 }
